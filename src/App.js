3ca31ec6a6d278ed31ec718a74c00859ac75e5a2
seni7 <85029836+seni7@users.noreply.github.com>
2022-09-19 16:38:04 +0300

    upgradded

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './componants/Header';
+import Cover from './componants/Cover'
+import About from './componants/About';
+import Info from './componants/Info'
+import Footer from './componants/Footer';
+import Slider  from './componants/Slider';
+function App() {
+  return (
+    
+    <Router>
+    <div className="App">
+     <Routes>
+          <Route path="/about" element={<><Header/><About/></>}/>
+
+          <Route path="/info" element={<><Header/><Info/></>}/>
+
+          <Route path="/skill" element={<><Header/><Footer/></>}/>
+
+          <Route path="/" 
+          element={ <><Header/><Cover/><About/><Slider/><Info/><Footer/></>
+          }/>
+     </Routes>
+     </div>
+    </Router>
+
+  );
+}
+
+export default App;
